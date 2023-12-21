b6e471fadb858bb64acf2bf98e2b9c8a0890e7a9
Yad Smood <y.s.inside@gmail.com>
2023-12-21 16:58:25 +0800

    update doc

--- example/basic.go
+++ example/basic.go
@@ -7,28 +7,30 @@ import (
 	_ "github.com/ysmood/goe/load" // load the .env file
 )
 
+var (
+	// Get a optional env variable.
+	num = goe.Get("NUM", 0)
+
+	// Get a required env variable.
+	str = goe.Require[string]("STR")
+
+	// Check if the env variable is equal to specified value.
+	isDev = goe.Is("ENV", "dev")
+
+	// Get a list from a env variable.
+	list = goe.GetList("LIST", []int{})
+
+	// Get a map from a env variable.
+	numMap = goe.GetMap("MAP", map[int]int{})
+
+	// Get a expanded env variable from expression like EXPANDED=${ENV}.
+	expanded = goe.Get("EXPANDED", "")
+
+	// Get a custom type from env variable.
+	time = goe.RequireWithParser("TIME", goe.Time)
+)
+
 func main() {
-	// Get a optional env variable
-	num := goe.Get("NUM", 0)
-
-	// Get a required env variable
-	str := goe.Require[string]("STR")
-
-	// Check if the env variable is equal to specified value
-	isDev := goe.Is("ENV", "dev")
-
-	// Get a list from a env variable
-	list := goe.GetList("LIST", []int{})
-
-	// Get a map from a env variable
-	numMap := goe.GetMap("MAP", map[int]int{})
-
-	// Get a expanded env variable from expression like EXPANDED=${ENV}
-	expanded := goe.Get("EXPANDED", "")
-
-	// Get a custom type from env variable
-	time := goe.RequireWithParser("TIME", goe.Time)
-
 	// It will output the env variables in "../.env"
 	// Output:
 	//	1 hello true [1 2] map[1:2 3:4] dev 2023
