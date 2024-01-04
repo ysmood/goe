e837e19454ddfb4eeeb99842a9d4600d1c1b708b
Yad Smood <y.s.inside@gmail.com>
2024-01-04 16:24:45 +0800

    update deps

--- constants.go
+++ constants.go
@@ -2,7 +2,7 @@ package goe
 
 const (
 	DOTENV          = ".env"
-	WHISPER         = "github.com/ysmood/whisper@v0.3.11"
+	WHISPER         = "github.com/ysmood/whisper@v0.3.12"
 	GOE_ENV_VIEWERS = "GOE_ENV_VIEWERS"
 	GOE_FILE_EXT    = ".goe"
 )
