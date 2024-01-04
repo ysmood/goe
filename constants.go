1434a0cc9f9c7be7170e49307d2a38eb2158375c
Yad Smood <y.s.inside@gmail.com>
2024-01-04 13:39:33 +0800

    fix Windows

--- constants.go
+++ constants.go
@@ -2,7 +2,7 @@ package goe
 
 const (
 	DOTENV          = ".env"
-	WHISPER         = "github.com/ysmood/whisper@v0.3.9"
+	WHISPER         = "github.com/ysmood/whisper@v0.3.11"
 	GOE_ENV_VIEWERS = "GOE_ENV_VIEWERS"
 	GOE_FILE_EXT    = ".goe"
 )
--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/ysmood/goe
 
 go 1.21
 
-require github.com/ysmood/got v0.38.2
+require github.com/ysmood/got v0.39.2
 
 require (
 	github.com/ysmood/gop v0.2.0 // indirect
